5022f133181e91f876f4928457a4513438ad80ef
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-01 12:34:31 +0700

    db: add user model interface

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,4 +1,14 @@
-import { Schema, model, models } from 'mongoose';
+import { Document, Schema, model, models } from 'mongoose';
+
+export interface IUser extends Document {
+  _id: string;
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+}
 
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
@@ -11,4 +21,4 @@ const UserSchema = new Schema({
 
 const User = models.User || model('User', UserSchema);
 
-export default User
+export default User;
