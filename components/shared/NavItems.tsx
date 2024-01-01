09d61a5b4092292efc9b755624ccc6ca19a1243b
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-01 20:31:49 +0700

    style: add font-bold to nav items using path name

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,22 +1,33 @@
+'use client';
+
 import { headerLinks } from '@/constants';
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import { usePathname } from 'next/navigation';
 
 export default function NavItems() {
+  const pathname = usePathname();
+
   return (
     <ul className="flex flex-col sm:flex-row gap-4">
-      {headerLinks.map((link) => (
-        <li key={link.route}>
-          <Button
-            variant="ghost"
-            size="lg"
-            className="w-full justify-start pl-2 sm:px-3"
-            asChild
-          >
-            <Link href={link.route}>{link.label}</Link>
-          </Button>
-        </li>
-      ))}
+      {headerLinks.map((link) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <li key={link.route}>
+            <Button
+              variant="ghost"
+              size="lg"
+              className={`${
+                isActive && 'font-bold'
+              } "w-full justify-start pl-2 sm:px-3"`}
+              asChild
+            >
+              <Link href={link.route}>{link.label}</Link>
+            </Button>
+          </li>
+        );
+      })}
     </ul>
   );
 }
