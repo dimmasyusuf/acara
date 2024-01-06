55e12f44a708bbb3896f40bbf1d7555a88a43141
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-06 23:59:26 +0700

    style: create event card ui

--- components/shared/Hero.tsx
+++ components/shared/Hero.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Hero() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4">
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4 mb-8">
       <div className="flex justify-center items-center order-2 md:order-1">
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-center md:text-left mb-2 sm:mb-4 w-full">
